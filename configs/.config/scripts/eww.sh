0f1b0e5c99a2cbcf78a78535d6473d6f910dbb67
NguyenThanhDuyOfficial <nguyenthanhduyofficial@gmail.com>
2025-09-21 02:38:19 +0700

    refactor(statusbar): review and cleanup

--- configs/.config/scripts/eww.sh
+++ configs/.config/scripts/eww.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/bash
 
-./volume.sh -i
+scripts=~/.config/scripts/
+
+sleep 1s
+$scripts/volume.sh -i
+$scripts/bright.sh -i
+
+$scripts/workspaces.sh ## Always leave this line in the bottom
---
+++ configs/.config/scripts/workspaces.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# can i make this script more efficient?
+activeWorkspace=0
+workspaces=(0 0 0 0 0 0)
+
+handle() {
+  case $1 in
+  "workspace>>"*)
+
+    workspaces[$activeWorkspace - 1]=0
+    activeWorkspace="${1#*>>}"
+    workspaces[$activeWorkspace - 1]=1
+
+    json_array=$(
+      IFS=,
+      echo "[${workspaces[*]}]"
+    )
+
+    eww update workspaceArray=$json_array
+    ;;
+  esac
+}
+
+socat -U - UNIX-CONNECT:$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock | while read -r line; do handle "$line"; done
